fdc65ef758c19eaf7e7b0b309913779ccc958470
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-04 15:45:36 +0000

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from utils import read_features
+from utils import read_features, tab_printer
 from admm_nmf import ADMM_NMF
 from parser import parameter_parser
 
 def execute_factorization():
     args = parameter_parser()
+    tab_printer(args)
     X = read_features(args.input_path)
+    print("\nTraining started.\n")
     model = ADMM_NMF(X, args)
     model.optimize()
+    print("\nFactors saved.\n")
     model.save_user_factors()
     model.save_item_factors()
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,21 @@
 import json
 import numpy as np
 import pandas as pd
-import networkx as nx
 from scipy import sparse
+from texttable import Texttable
+
+
+
+def tab_printer(args):
+    """
+    Function to print the logs in a nice tabular format.
+    :param args: Parameters used for the model.
+    """
+    args = vars(args)
+    keys = sorted(args.keys())
+    t = Texttable() 
+    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+    print(t.draw())
 
 def read_features(path):
 
