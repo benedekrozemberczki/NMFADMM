7832e0f91f3bf61825cf5337782042f49030efb4
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-05 21:57:23 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -22,13 +22,10 @@ def read_features(path):
     :Return features: Target matrix.
     """
     features = pd.read_csv(path)
-    
     index_1 = features["index_1"].values.tolist()
     index_2 = features["index_2"].values.tolist()
     values = features["values"].values.tolist()
-    
     users = max(index_1)+1
     items = max(index_2)+1
-    
     features = sparse.csr_matrix(sparse.coo_matrix((values,(index_1,index_2)),shape=(users,items),dtype=np.float32))
     return features
