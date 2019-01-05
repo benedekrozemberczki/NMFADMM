848e82b945319cc043b7ed162cb68e2db8387aa5
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-05 21:56:30 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,6 @@ import pandas as pd
 from scipy import sparse
 from texttable import Texttable
 
-
-
 def tab_printer(args):
     """
     Function to print the logs in a nice tabular format.
@@ -18,15 +16,19 @@ def tab_printer(args):
     print(t.draw())
 
 def read_features(path):
-
+    """
+    Reading the sparse matrix.
+    :param path: Path to the sparse matrix.
+    :Return features: Target matrix.
+    """
     features = pd.read_csv(path)
-
+    
     index_1 = features["index_1"].values.tolist()
     index_2 = features["index_2"].values.tolist()
     values = features["values"].values.tolist()
-
+    
     users = max(index_1)+1
     items = max(index_2)+1
-
+    
     features = sparse.csr_matrix(sparse.coo_matrix((values,(index_1,index_2)),shape=(users,items),dtype=np.float32))
     return features
