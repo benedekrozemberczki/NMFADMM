ebc58b723f2601b5ff2edd2b2ca090c225fa82e6
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2019-01-05 21:59:15 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from utils import read_features, tab_printer
 from admm_nmf import ADMM_NMF
 from parser import parameter_parser
+from utils import read_features, tab_printer
 
 def execute_factorization():
+    """
+    Reading the target matrix, running optimization and saving to hard drive.
+    """
     args = parameter_parser()
     tab_printer(args)
     X = read_features(args.input_path)
@@ -14,6 +17,4 @@ def execute_factorization():
     model.save_item_factors()
 
 if __name__ == '__main__':
-
     execute_factorization()
-
